3aa24ad4bd024c2d5ae39358c96366b5d9fab1aa
ruheni <aleruheni@outlook.com>
2020-01-30 07:57:18 +0300

    added form builder, group, form control to class and subclass components

--- src/app/setups/class/class.component.ts
+++ src/app/setups/class/class.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-class',
-  templateUrl: './class.component.html',
-  styleUrls: ['./class.component.scss']
+    selector: 'app-class',
+    templateUrl: './class.component.html',
+    styleUrls: ['./class.component.scss']
 })
 export class ClassComponent implements OnInit {
+    classDetails: FormGroup;
+    constructor(private fb: FormBuilder) {
+        this.classDetails = fb.group({
+            classCode: [''],
+            classDetails: [''],
+            insuranceAccLimit: [],
+            policyAccLimit: [],
+            withEffectFrom: [],
+            withEffectTo: []
+        });
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
--- src/app/setups/subclass/subclass.component.ts
+++ src/app/setups/subclass/subclass.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-subclass',
-  templateUrl: './subclass.component.html',
-  styleUrls: ['./subclass.component.scss']
+    selector: 'app-subclass',
+    templateUrl: './subclass.component.html',
+    styleUrls: ['./subclass.component.scss']
 })
 export class SubclassComponent implements OnInit {
+    subClassDetails: FormGroup;
+    constructor(private fb: FormBuilder) {
+        this.subClassDetails = fb.group({
+            subClassName: [''],
+            withEffectFrom: [],
+            withEffectTo: [],
+            claimsNoPrefix: [],
+            propertyId: [],
+            platformAdminCode: []
+        });
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
